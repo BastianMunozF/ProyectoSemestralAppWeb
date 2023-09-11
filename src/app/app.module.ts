6cd6d2620fcefeb2e42af7e356e51a042f72af3e
BastianMunozF <ba.munozf@duocuc.cl>
2023-09-11 01:18:39 -0300

    Ruta conductor arreglos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, MatSlideToggleModule],
