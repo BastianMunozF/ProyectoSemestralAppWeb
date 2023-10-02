72f133485943a187f5f9e9eb92ea9e5dd3798458
BastianMunozF <ba.munozf@duocuc.cl>
2023-10-02 20:01:32 -0300

    Validaciones registrar usuarios listas

--- src/app/pages/paginaregistrar-usuario/paginaregistrar-usuario.page.ts
+++ src/app/pages/paginaregistrar-usuario/paginaregistrar-usuario.page.ts
@@ -20,11 +20,11 @@ export class PaginaregistrarUsuarioPage implements OnInit {
     this.formularioRegistro = this.fb.group({
       'nombre': new FormControl("", [Validators.required, Validators.minLength(3), Validators.maxLength(30), Validators.pattern('[a-zA-Z]*')]),
       'apellido': new FormControl("", [Validators.required, Validators.minLength(3), Validators.maxLength(30), Validators.pattern('[a-zA-Z]*')]),
-      'rut': new FormControl("", [Validators.required, Validators.minLength(12), Validators.maxLength(13), Validators.pattern('[0-9K.-]*')]),
-      'edad': new FormControl("", [Validators.required, Validators.minLength(2), Validators.maxLength(2), Validators.pattern('[0-9]*')]),
-      'correo': new FormControl("", [Validators.required, Validators.minLength(10), Validators.maxLength(30), Validators.pattern('[a-zA-Z@.#$%]*'), Validators.email]),
+      'rut': new FormControl("", [Validators.required, Validators.minLength(9), Validators.maxLength(13), Validators.pattern('[0-9]*'), Validators.pattern('[K.-]')]),
+      'edad': new FormControl("", [Validators.required, Validators.minLength(2), Validators.maxLength(2), Validators.pattern('[0-9]*'), Validators.min(12)]),
+      'correo': new FormControl("", [Validators.required, Validators.minLength(10), Validators.maxLength(50), Validators.pattern('[a-zA-Z@.#$%]*'), Validators.email]),
       'celular': new FormControl("", [Validators.required, Validators.minLength(9), Validators.maxLength(9), Validators.pattern('[0-9]*')]),
-      'password': new FormControl("", Validators.required),
+      'password': new FormControl("", [Validators.required, Validators.minLength(10), Validators.maxLength(50), Validators.pattern('[a-z]'), Validators.pattern('[A-Z]'), Validators.pattern('[0-9]'), Validators.pattern('[!@#$%^&*()-=+_]')]),
       'confirmacionPassword': new FormControl("", Validators.required)
     });
 
