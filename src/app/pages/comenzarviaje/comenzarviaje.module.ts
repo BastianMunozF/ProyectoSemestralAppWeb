dbdbd5f5d3670d1323b9163d260b138a87b9c1a0
ScarlettRosales <sc.rosales@duocuc.cl>
2023-10-15 22:19:22 -0300

    Nuevo menú

--- src/app/pages/comenzarviaje/comenzarviaje.module.ts
+++ src/app/pages/comenzarviaje/comenzarviaje.module.ts
@@ -7,13 +7,18 @@ import { IonicModule } from '@ionic/angular';
 import { ComenzarviajePageRoutingModule } from './comenzarviaje-routing.module';
 
 import { ComenzarviajePage } from './comenzarviaje.page';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ComenzarviajePageRoutingModule
+    ComenzarviajePageRoutingModule,
+    MatButtonModule, MatDividerModule, MatIconModule
   ],
   declarations: [ComenzarviajePage]
 })
