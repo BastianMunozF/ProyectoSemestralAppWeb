aeb081fe0d601c578431bd8431bb470a1c83dd82
BastianMunozF <ba.munozf@duocuc.cl>
2023-10-16 13:41:23 -0300

    Arreglo de inicio de sesión

--- src/app/pages/paginalogin-usuario/paginalogin-usuario.page.ts
+++ src/app/pages/paginalogin-usuario/paginalogin-usuario.page.ts
@@ -42,13 +42,14 @@ export class PaginaloginUsuarioPage implements OnInit {
           localStorage.setItem('id', usuario.id);
           localStorage.setItem('rol', usuario.id_rol);
 
-          let user_rol = localStorage.getItem('id');
+          let user_rol = localStorage.getItem('rol');
+          let user = JSON.stringify(user_rol);
 
-          if(user_rol = '1'){
+          if(user = '1'){
             this.router.navigate(['/menuprincipal']).then(() => {
               this.presentarAlerta("Sesión iniciada", "La sesión ha sido iniciada con éxito.");
             })
-          } else if (user_rol = '2'){
+          } else if (user = '2'){
             this.router.navigate(['/menuprincipalconductor']).then(() => {
               this.presentarAlerta("Sesión iniciada", "La sesión de conductor ha sido iniciada con éxito.");
             })
