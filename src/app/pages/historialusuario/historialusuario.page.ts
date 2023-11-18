f6704f7ef7c1edaf181bedaf2165a1fd59f2dc47
BastianMunozF <ba.munozf@duocuc.cl>
2023-11-18 17:53:40 -0300

    Arreglo en historial usuario

--- src/app/pages/historialusuario/historialusuario.page.ts
+++ src/app/pages/historialusuario/historialusuario.page.ts
@@ -15,11 +15,9 @@ export class HistorialusuarioPage implements OnInit {
   constructor(private database: DbserviceService, private router: Router) { }
 
   ngOnInit() {
-    /*
     this.database.buscarViajeUser(this.id_usuario).then(res => {
       this.arregloHistorial = res;
     });
-    */
   }
 
 }
