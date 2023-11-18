45b35689cf3149934ef0c368394a7b9af30c79b2
BastianMunozF <ba.munozf@duocuc.cl>
2023-11-17 21:27:02 -0300

    Arreglo de token

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -19,9 +19,8 @@ export class ApiService {
   constructor(private http: HttpClient) { }
 
   getClima(fechahora: any, latitud: any, longitud: any): Observable<any>{
-    return this.http.get(this.apiURL + fechahora + '/t_2m:C/' + latitud + ',' + longitud + '/json?access_token=eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2IjoxLCJ1c2VyIjoiZHVvY3VjX211bm96X2Jhc3RpYW4iLCJpc3MiOiJsb2dpbi5tZXRlb21hdGljcy5jb20iLCJleHAiOjE3MDAyNjU2NjUsInN1YiI6ImFjY2VzcyJ9.7ZmANURUPb7C4U41pQPNYLvJO_J5zhmwYEOGyOubFviPdM0QoOtTjadsElMRXGjtfd8tRJ3RXaj6dE2kU6F8uw').pipe(
+    return this.http.get(this.apiURL + fechahora + '/t_2m:C/' + latitud + ',' + longitud + '/json?access_token=eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2IjoxLCJ1c2VyIjoiZHVvY3VjX211bm96X2Jhc3RpYW4iLCJpc3MiOiJsb2dpbi5tZXRlb21hdGljcy5jb20iLCJleHAiOjE3MDAyNzQzNzMsInN1YiI6ImFjY2VzcyJ9.5X2Cm794LcG3VbYBvPccQ_NLLxzkzWm1U7QwCHKcEtIwl1RResJVY19YpRQcs5-si858XkhoBrveIlGEXe00Hg').pipe(
       retry(3)
     );
   }
-
 }
