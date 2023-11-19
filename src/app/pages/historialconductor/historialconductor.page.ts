4039fb82330319f1679fc423eaa1611ae39ebdb1
BastianMunozF <ba.munozf@duocuc.cl>
2023-11-19 15:35:59 -0300

    Arreglo en historialconductor.ts

--- src/app/pages/historialconductor/historialconductor.page.ts
+++ src/app/pages/historialconductor/historialconductor.page.ts
@@ -24,39 +24,38 @@ export class HistorialconductorPage implements OnInit {
 
         //Guardamos id's de los detalles del viaje
         this.detallesViaje = detalle;
-
-        this.database.buscarDatosUsuario(this.detallesViaje.id_usuario).then(usuario => {
-          if(usuario){
-
-            //Guardamos datos del usuario que ha pedido el viaje
-            this.arregloUsuario = usuario;
-
-            this.database.buscarViajeUser(this.detallesViaje.id_usuario).then(viaje => {
-              if(viaje){
-
-                //Guardamos los datos del viaje aceptado
-                this.arregloViajes = viaje;
-
-                this.database.buscarVehiculoUsuario(id_conductor).then(vehiculo => {
-                  if(vehiculo){
-                    
-                    //Guardamos datos del vehiculo utilizado por el conductor
-                    this.arregloVehiculo = vehiculo;
-
-                  } else {
-                    console.log('Error al buscar vehiculo.');
-                  }
-                })
-              } else {
-                console.log('Error al buscar viaje.');
-              }
-            })
-          } else {
-            console.log('Error al buscar usuario.')
-          }
-        })
       } else {
         this.presentarAlerta('Viajes no encontrados', 'Usted aún no ha aceptado ningún viaje.');
+      }
+    });
+
+    this.database.buscarDatosUsuario(this.detallesViaje.id_usuario).then(usuario => {
+      if(usuario){
+
+        //Guardamos datos del usuario que ha pedido el viaje
+        this.arregloUsuario = usuario;
+      } else {
+        console.log('Error al buscar usuario.');
+      }
+    });
+
+    this.database.buscarViajeUser(this.detallesViaje.id_usuario).then(viaje => {
+      if(viaje){
+        //Guardamos los datos del viaje aceptado
+        this.arregloViajes = viaje;
+      } else {
+        console.log('Error al buscar viaje.');
+      }
+    });
+
+    this.database.buscarVehiculoUsuario(id_conductor).then(vehiculo => {
+      if(vehiculo){
+        
+        //Guardamos datos del vehiculo utilizado por el conductor
+        this.arregloVehiculo = vehiculo;
+
+      } else {
+        console.log('Error al buscar vehiculo.');
       }
     })
   }
