a83b105baebfa9004eb7257b38e3fa0defef5464
BastianMunozF <ba.munozf@duocuc.cl>
2023-11-19 02:17:02 -0300

    Historial conductor + ionic cap sync

--- src/app/pages/historialconductor/historialconductor.page.ts
+++ src/app/pages/historialconductor/historialconductor.page.ts
@@ -28,40 +28,38 @@ export class HistorialconductorPage implements OnInit {
       } else {
         this.presentarAlerta('Viajes no encontrados', 'Usted aún no ha aceptado ningún viaje.');
       }
-    })
-  }
+    });
 
-  ionViewWillEnter(){
     this.database.buscarDatosUsuario(this.detallesViaje.id_usuario).then(usuario => {
       if(usuario){
 
         //Guardamos datos del usuario que ha pedido el viaje
         this.arregloUsuario = usuario;
 
-        this.database.buscarViajeUser(this.detallesViaje.id_usuario).then(viaje => {
-          if(viaje){
-
-            //Guardamos los datos del viaje aceptado
-            this.arregloViajes = viaje;
-
-            let id_conductor = localStorage.getItem('id');
-
-            this.database.buscarVehiculoUsuario(id_conductor).then(vehiculo => {
-              if(vehiculo){
-                
-                //Guardamos datos del vehiculo utilizado por el conductor
-                this.arregloVehiculo = vehiculo;
-
-              } else {
-                console.log('Error al buscar vehiculo.');
-              }
-            })
-          } else {
-            console.log('Error al buscar viaje.');
-          }
-        })
+        
       } else {
         console.log('Error al buscar usuario.')
+      }
+    });
+
+    this.database.buscarViajeUser(this.detallesViaje.id_usuario).then(viaje => {
+      if(viaje){
+    
+        //Guardamos los datos del viaje aceptado
+        this.arregloViajes = viaje;
+      } else {
+        console.log('Error al buscar viaje.');
+      }
+    });
+
+    this.database.buscarVehiculoUsuario(id_conductor).then(vehiculo => {
+      if(vehiculo){
+
+        //Guardamos datos del vehiculo utilizado por el conductor
+        this.arregloVehiculo = vehiculo;
+
+      } else {
+        console.log('Error al buscar vehiculo.');
       }
     })
   }
