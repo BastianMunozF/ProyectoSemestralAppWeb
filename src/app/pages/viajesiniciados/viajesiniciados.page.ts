3083d0f02c11dc589f840053c4a25bd742ecf98b
BastianMunozF <ba.munozf@duocuc.cl>
2023-12-07 02:58:32 -0300

    Arreglo en menu conductor, viajes creados y espacios en tomar viaje

--- src/app/pages/viajesiniciados/viajesiniciados.page.ts
+++ src/app/pages/viajesiniciados/viajesiniciados.page.ts
@@ -66,33 +66,43 @@ export class ViajesiniciadosPage implements OnInit {
   ngOnInit() {
     let id_user = localStorage.getItem('id');
 
-    this.database.buscarViajeUser(id_user).then(viaje => {
-      if(viaje){
+    this.database.buscarViajeUser(id_user).then(data => {
+      if(data.length > 0){
 
-        this.arregloViajes = viaje;
-
-        this.database.buscarDetalleViaje(this.arregloViajes.id_viaje).then(detalle => {
-          if(detalle){
-
-            this.arregloDetalle = detalle;
-
-            this.database.buscarDatosUsuario(this.arregloDetalle.id_usuario).then(usuario => {
-              if(usuario){
-
-                this.arregloUsuario = usuario;
-
+        this.database.fetchViajeUser().subscribe(datos => {
+  
+          if(datos.length > 0){
+    
+            console.log('Viajes del usuario: ', datos);
+            this.arregloViajes = datos;
+    
+            this.database.buscarDetalleViaje(this.arregloViajes.id_viaje);
+    
+            this.database.fetchDetalleViaje().subscribe(detalle => {
+              if(detalle){
+    
+                console.log('Detalle del viaje: ', detalle);
+                this.arregloDetalle = detalle;
+    
+                this.database.buscarDatosUsuario(this.arregloDetalle.id_usuario).then(usuario => {
+                  if(usuario){
+    
+                    this.arregloUsuario = usuario;
+    
+                  }
+                });
               }
-
-            });
-
+            })
+    
+          } else {
+    
+            this.presentarAlerta("Error al cargar viajes", "No se han encontrado viajes.");
+    
           }
-        
+    
         })
-
       } else {
-
         this.presentarAlerta("Error al cargar viajes", "Usted aún no ha creado viajes.");
-
       }
     })
   }
