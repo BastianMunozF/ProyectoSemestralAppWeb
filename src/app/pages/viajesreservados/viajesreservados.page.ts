9484b1ee3f567c68a7f19ebc91d3d3a2d7a7f91c
BastianMunozF <ba.munozf@duocuc.cl>
2023-12-09 16:23:08 -0300

    Arreglo

--- src/app/pages/viajesreservados/viajesreservados.page.ts
+++ src/app/pages/viajesreservados/viajesreservados.page.ts
@@ -59,52 +59,6 @@ export class ViajesreservadosPage implements OnInit {
     }
   ]
 
-  arregloReserva: any = [
-    {
-      detalle:
-      {
-        id_detalle: '',
-        id_usuario: '',
-        id_viaje: '',
-      },
-      usuario:
-      {
-        id_usuario: '',
-        nombre: '',
-        apellido: '',
-        correo: '',
-        fechanacimiento: '',
-        rut: '',
-        celular: '',
-        contrasena: '',
-        fotoperfil: '',
-      },
-      vehiculo:
-      {
-        id_vehiculo: '',
-        marca: '',
-        modelo: '',
-        anio: '',
-        patente: '',
-        asientos: '',
-        id_usuario: '',
-        id_tipo: '',
-      },
-      viaje:
-      {
-        id_viaje: '',
-        f_viaje: '',
-        hora_salida: '',
-        salida: '',
-        destino: '',
-        cant_asientos: '',
-        valor_asiento: '',
-        estado: '',
-        id_usuario: '',
-      }
-    }
-  ]
-
   constructor(private database: DbserviceService, private alertController: AlertController) { }
 
   ngOnInit() {
@@ -119,46 +73,36 @@ export class ViajesreservadosPage implements OnInit {
         this.database.fetchDetalleUser().subscribe(detail => {
           if(detail.length > 0){
             this.arregloDetalle = detalle;
+          }
+        })
+      }
+    })
 
-            this.database.buscarViajeReservado(detalle[0].id_viaje, estado).then(viaje => {
-              if(viaje.length > 0){
-                this.database.fetchViajeReservado().subscribe(viajes => {
-                  if(viajes.length > 0){
-                    this.arregloViajes = viaje;
+    this.database.buscarViajeReservado(parseInt(this.arregloDetalle.id_viaje), estado).then(viaje => {
+      if(viaje.length > 0){
+        this.database.fetchViajeReservado().subscribe(viajes => {
+          if(viajes.length > 0){
+            this.arregloViajes = viaje;
+          }
+        })
+      }
+    })
 
-                    this.database.buscarDatosUsuario(viaje[0].id_usuario).then(usuario => {
-                      if(usuario.length > 0){
-                        this.database.fetchUsuarioId().subscribe(usuarios => {
-                          if(usuarios.length > 0){
-                            this.arregloUsuario = usuario;
+    this.database.buscarDatosUsuario(parseInt(this.arregloViajes.id_usuario)).then(usuario => {
+      if(usuario.length > 0){
+        this.database.fetchUsuarioId().subscribe(usuarios => {
+          if(usuarios.length > 0){
+            this.arregloUsuario = usuario;
+          }
+        })
+      }
+    })
 
-                            this.database.buscarVehiculoUsuario(usuario[0].id).then(vehiculo => {
-                              if(vehiculo.length > 0){
-                                for(var i = 0; i < this.arregloDetalle.length; i++){
-                                  for(var j = 0; j < this.arregloUsuario.length; j++){
-                                    for(var k = 0; k < this.arregloViajes.length; k++){
-                                      for(var l = 0; l < vehiculo.length; l++){
-                                        reserva.push({
-                                          detalle: this.arregloDetalle[i],
-                                          usuario: this.arregloUsuario[j],
-                                          vehiculo: vehiculo[l],
-                                          viaje: this.arregloViajes[k]
-                                        })
-                                      }
-                                    }
-                                  }
-                                }
-                                return this.arregloReserva.push(reserva);
-                              }
-                            })
-                          }
-                        })
-                      }
-                    })
-                  }
-                })
-              }
-            })
+    this.database.buscarVehiculoUsuario(parseInt(this.arregloUsuario.id_usuario)).then(vehiculo => {
+      if(vehiculo.length > 0){
+        this.database.fetchVehiculoUser().subscribe(vehiculos => {
+          if(vehiculos.length > 0){
+            this.arregloVehiculo = vehiculo;
           }
         })
       }
