2aee5c1880f844d9c980bac626296f20c2d1e6a0
BastianMunozF <ba.munozf@duocuc.cl>
2023-12-09 01:22:17 -0300

    Intento raro

--- src/app/pages/viajesreservados/viajesreservados.page.ts
+++ src/app/pages/viajesreservados/viajesreservados.page.ts
@@ -59,6 +59,52 @@ export class ViajesreservadosPage implements OnInit {
     }
   ]
 
+  arregloReserva: any = [
+    {
+      detalle:
+      {
+        id_detalle: '',
+        id_usuario: '',
+        id_viaje: '',
+      },
+      usuario:
+      {
+        id_usuario: '',
+        nombre: '',
+        apellido: '',
+        correo: '',
+        fechanacimiento: '',
+        rut: '',
+        celular: '',
+        contrasena: '',
+        fotoperfil: '',
+      },
+      vehiculo:
+      {
+        id_vehiculo: '',
+        marca: '',
+        modelo: '',
+        anio: '',
+        patente: '',
+        asientos: '',
+        id_usuario: '',
+        id_tipo: '',
+      },
+      viaje:
+      {
+        id_viaje: '',
+        f_viaje: '',
+        hora_salida: '',
+        salida: '',
+        destino: '',
+        cant_asientos: '',
+        valor_asiento: '',
+        estado: '',
+        id_usuario: '',
+      }
+    }
+  ]
+
   constructor(private database: DbserviceService, private alertController: AlertController) { }
 
   ngOnInit() {
@@ -68,52 +114,61 @@ export class ViajesreservadosPage implements OnInit {
     // Buscar todos los detalles del usuario
     this.database.buscarDetalleUser(id_user).then(res => {
       if(res.length > 0){
-                
+  
         this.arregloDetalle = res;
   
         this.arregloDetalle.forEach((detalle: any) => {
           // Buscar todos los viajes reservados por el usuario
           this.database.buscarViajeReservado(detalle.id_viaje, estado).then(res => {
             if(res.length > 0){
-    
+  
               this.arregloViajes = res;
-    
+  
               // Buscar todos los usuarios
               this.database.buscarDatosUsuario(this.arregloViajes.id_usuario).then(res => {
                 if(res.length > 0){
-    
+  
                   this.arregloUsuario = res;
-    
+  
                   // Buscar todos los vehiculos
                   this.database.buscarVehiculoUsuario(this.arregloUsuario.id_usuario).then(res => {
                     if(res.length > 0){
-    
+  
                       this.arregloVehiculo = res;
-    
+  
+                      // Crear un objeto temporal y empujarlo al arregloReserva
+                      let reserva = {
+                        detalle: detalle,
+                        usuario: this.arregloUsuario[0],
+                        vehiculo: this.arregloVehiculo[0],
+                        viaje: this.arregloViajes[0]
+                      };
+  
+                      this.arregloReserva.push(reserva);
+  
                     } else {
-    
+  
                       console.log('No se han encontrado vehiculos.');
-    
+  
                     }
                   })
-    
+  
                 } else {
-    
+  
                   console.log('No se han encontrado usuarios.');
-    
+  
                 }
               })
-    
+  
             } else {
-    
+  
               console.log('No se han encontrado viajes.');
-    
+  
             }
           })
         })
       }
     })
-      
   }
 
   cancelarReserva(viaje: any){
