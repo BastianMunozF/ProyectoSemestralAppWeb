183bcb459ea6cdeaa479aadaa1a84e75bf28f495
BastianMunozF <ba.munozf@duocuc.cl>
2024-06-23 18:06:13 -0400

    Creo que ahora

--- src/app/services/api-flow.service.ts
+++ src/app/services/api-flow.service.ts
@@ -1,20 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient, HttpParams } from '@angular/common/http';
 import * as crypto from 'crypto-js';
-import { Observable, catchError, retry } from 'rxjs';
+import { Observable, catchError, retry, firstValueFrom } from 'rxjs';
 import { AlertController } from '@ionic/angular';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiFlowService {
-
-  httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*'
-    })
-  }
 
   url = 'https://www.flow.cl/api';
   secretKey = '2b29f9a892dbfb86067cdda2123753e1d2b3db74';
@@ -29,32 +22,29 @@ export class ApiFlowService {
 
   async crearOrdenPago(params: any): Promise<any> {
     try {
-        const firma = this.firmarParametros(params);
-        params['s'] = firma;
-        
-        // Construir el cuerpo de la solicitud
-        let body = new HttpParams();
-        for (const key in params) {
-            if (params.hasOwnProperty(key)) {
-                body = body.append(key, params[key]);
-            }
-        }
-        
-        // Configurar los encabezados
-        const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
-        
-        // Enviar la solicitud y esperar la respuesta
-        this.presentarAlerta("Esta sería la URL", "URL" + this.http.post<any>(`${this.url}/payment/create/`, body.toString(), { headers }).toPromise().toString())
-
-        const response = await this.http.post<any>(`${this.url}/payment/create/`, body.toString(), { headers }).toPromise();
-
-        this.presentarAlerta("Response crearOrdenPago()", "Response " + JSON.stringify(response))
-        console.log('Respuesta de crearOrdenPago:', response); // Imprime la respuesta para verificar
-        
-        return response;
+      const firma = this.firmarParametros(params);
+      params['s'] = firma;
+  
+      const body = new HttpParams({ fromObject: params });
+      const headers = new HttpHeaders({ 'Content-Type': 'application/x-www-form-urlencoded' });
+  
+      const response = await firstValueFrom(
+        this.http.post<any>(`${this.url}/payment/create`, body.toString(), { headers })
+          .pipe(
+            retry(3),
+            catchError(error => {
+              console.error('Error en la solicitud crearOrdenPago:', error);
+              throw error;
+            })
+          )
+      );
+  
+      console.log('Respuesta de crearOrdenPago:', JSON.stringify(response));
+  
+      return response;
     } catch (error) {
-        console.error('Error en la solicitud crearOrdenPago:', error);
-        throw error; // Lanza el error para manejarlo en el componente que llama a esta función
+      console.error('Error en la solicitud crearOrdenPago:', error);
+      throw error; // Lanza el error para manejarlo en el componente que llama a esta función
     }
   }
 
@@ -67,7 +57,14 @@ export class ApiFlowService {
         httpParams = httpParams.append(key, params[key]);
       }
     }
-    return this.http.get<any>(`${this.url}/payment/getStatus`, { params: httpParams }).pipe(retry(3));
+    return this.http.get<any>(`${this.url}/payment/getStatus`, { params: httpParams })
+      .pipe(
+        retry(3),
+        catchError(error => {
+          console.error('Error en obtenerPago:', error);
+          throw error;
+        })
+      );
   }
 
   async presentarAlerta(titulo: string, mensaje: string) {
