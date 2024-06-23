89162e3ca0e9d7538ea54f1bacc4333be0b2067a
BastianMunozF <ba.munozf@duocuc.cl>
2024-06-22 21:30:29 -0400

    Arreglo, creo que funciona ahora

--- src/app/pages/historialusuario/historialusuario.page.ts
+++ src/app/pages/historialusuario/historialusuario.page.ts
@@ -34,20 +34,6 @@ export class HistorialusuarioPage implements OnInit {
     }
   ]
 
-  arregloUser: any = [
-    {
-      id: '',
-      nombre: '',
-      apellido: '',
-      correo: '',
-      fechanacimiento: '',
-      rut: '',
-      celular: '',
-      contrasena: '',
-      fotoperfil: '',
-    }
-  ]
-
   arregloVehiculo: any = [
     {
       id_vehiculo: '',
@@ -75,83 +61,23 @@ export class HistorialusuarioPage implements OnInit {
     }
   ]
 
+  arregloViajesFirma: any = [
+    {
+      id_viaje: '',
+      f_viaje: '',
+      hora_salida: '',
+      salida: '',
+      destino: '',
+      cant_asientos: '',
+      valor_asiento: '',
+      estado: '',
+      id_usuario: '',
+    }
+  ]
+
   transaccion: any;
 
   constructor(private database: DbserviceService, private alertController: AlertController, private apiFlow: ApiFlowService) { }
-
-  async postFlow() {
-    const id_user = localStorage.getItem('id');
-    if (!id_user) {
-      this.presentarAlerta('Error', 'No se encontró el ID del usuario.');
-      return;
-    }
-  
-    try {
-      const datos = await this.database.buscarDatosUsuario(id_user);
-      if (datos.length > 0) {
-        this.arregloUser = datos;
-      }
-
-      const token = 'ASDFGHJKL123'
-
-      const paramsGet = {
-        apiKey: '1F8DDF83-C842-41A6-8A41-5D848L6E0AC0',
-        tokenFlow: token
-      }
-
-      const secretKey = this.apiFlow.firmarParametros(paramsGet)
-  
-      const params = {
-        apiKey: '1F8DDF83-C842-41A6-8A41-5D848L6E0AC0',
-        commerceOrder: 'ORDEN' + this.arregloDetalle.id_detalle,
-        subject: 'Pago de Viaje',
-        amount: this.arregloViajes.length > 0 ? this.arregloViajes[0].valor_asiento : 0,
-        email: this.arregloUser.length > 0 ? this.arregloUser[0].correo : '',
-        paymentMethod: 9,
-        urlConfirmation: 'https://proyecto-semestral-app-web.vercel.app/historialusuario',
-        urlReturn: 'https://proyecto-semestral-app-web.vercel.app/historialusuario',
-        timeout: 3600,
-        s: secretKey
-      };
-  
-      const response = await this.apiFlow.crearOrdenPago(params).toPromise();
-  
-      if (response && response.url && response.token) {
-        const redirectUrl = `${response.url}?token=${response.token}`;
-        window.location.href = redirectUrl;
-      } else {
-        this.presentarAlerta('Error en la transacción', 'No se recibió la URL de redirección.');
-      }
-    } catch (error) {
-      console.error('Error en la transacción:', error);
-      this.presentarAlerta('Error en la transacción', 'Ha ocurrido un error al momento de efectuar la transacción.');
-    }
-  }
-
-  getFlowStatus() {
-      if (!this.transaccion || !this.transaccion.token) {
-          console.error('Token de transacción no disponible.');
-          return;
-      }
-
-      const token = this.transaccion.token;
-
-      const paramsGet = {
-          apiKey: '1F8DDF83-C842-41A6-8A41-5D848L6E0AC0',
-          tokenFlow: token
-      };
-
-      this.apiFlow.obtenerPago(paramsGet).subscribe(res => {
-          if (res) {
-              console.log('Estado del pago:', res);
-          } else {
-              console.error('Ha ocurrido un error al obtener el estado del pago.');
-          }
-      }, error => {
-          console.error('Error en la obtención del estado del pago:', error);
-      });
-  }
-
 
   async ngOnInit() {
     let id_user = localStorage.getItem('id');
@@ -230,6 +156,73 @@ export class HistorialusuarioPage implements OnInit {
     }).catch(error => {
       console.log('Error en Buscar Detalle User: ', error);
     })
+  }
+
+  async postFlow(viaje: any) {
+  
+    try {
+
+      const paramsGet = {
+        "apiKey": '1F8DDF83-C842-41A6-8A41-5D848L6E0AC0',
+        "currency": 'CLP',
+        "amount": viaje.valor_asiento
+      }
+
+      const secretKey = this.apiFlow.firmarParametros(paramsGet);
+
+      console.log(secretKey)
+
+      let id_user = localStorage.getItem('id')
+
+      const params = {
+        apiKey: '1F8DDF83-C842-41A6-8A41-5D848L6E0AC0',
+        commerceOrder: 'ORDEN' + viaje.id_viaje,
+        subject: 'Pago de Viaje',
+        amount: viaje.valor_asiento,
+        email: id_user,
+        paymentMethod: 9,
+        urlConfirmation: 'https://proyecto-semestral-app-web.vercel.app/historialusuario',
+        urlReturn: 'https://proyecto-semestral-app-web.vercel.app/historialusuario',
+        timeout: 3600,
+        s: secretKey
+      };
+  
+      const response = await this.apiFlow.crearOrdenPago(params).toPromise();
+  
+      if (response && response.url && response.token) {
+        const redirectUrl = `${response.url}?token=${response.token}`;
+        window.location.href = redirectUrl;
+      } else {
+        this.presentarAlerta('Error en la transacción', 'No se recibió la URL de redirección.');
+      }
+    } catch (error) {
+      console.error('Error en la transacción:', error);
+      this.presentarAlerta('Error en la transacción', 'Ha ocurrido un error al momento de efectuar la transacción.');
+    }
+  }
+
+  getFlowStatus() {
+      if (!this.transaccion || !this.transaccion.token) {
+          console.error('Token de transacción no disponible.');
+          return;
+      }
+
+      const token = this.transaccion.token;
+
+      const paramsGet = {
+          apiKey: '1F8DDF83-C842-41A6-8A41-5D848L6E0AC0',
+          tokenFlow: token
+      };
+
+      this.apiFlow.obtenerPago(paramsGet).subscribe(res => {
+          if (res) {
+              console.log('Estado del pago:', res);
+          } else {
+              console.error('Ha ocurrido un error al obtener el estado del pago.');
+          }
+      }, error => {
+          console.error('Error en la obtención del estado del pago:', error);
+      });
   }
 
   async presentarAlerta(titulo: string, mensaje: string) {
